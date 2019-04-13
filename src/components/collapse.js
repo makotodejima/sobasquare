28211da8be137304f9514bfc4b4469b046bf7d1f
Makoto Dejima <makotodejima@gmail.com>
2019-04-13 18:47:50 +0900

    Fix classNmae

--- src/components/collapse.js
+++ src/components/collapse.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default () => (
   <svg id="collapse" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <path class="collapse" d="M83.08 67.97L50 32.03 16.92 67.97" />
+    <path className="collapse" d="M83.08 67.97L50 32.03 16.92 67.97" />
   </svg>
 );
