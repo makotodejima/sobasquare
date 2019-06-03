71d0629212c78699072505dbb5db1d3f6ec366d3
Makoto Dejima <makotodejima@gmail.com>
2019-06-03 15:41:28 +0200

    Footer md

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,39 @@ import styled from "styled-components";
 
 export default () => (
   <Footer>
-    <p>Designed and developed by Makoto Dejima</p>
-    {/* <p>
-      Contact information:{" "}
-      <a href="mailto:someone@example.com">someone@example.com</a>.
-    </p> */}
+    <div className="left">
+      <p>
+        Designed and developed by{" "}
+        <a href="https://madmak.me/"> Makoto Dejima</a>
+      </p>
+    </div>
+    <div className="right">
+      <p>Missing your favorite? Please let us know!</p>
+    </div>
   </Footer>
 );
 
 const Footer = styled.footer`
-  p {
-    font-size: 0.8rem;
-    margin-left: 1rem;
-  }
   position: fixed;
-  bottom: 0;
+  bottom: 3px;
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  font-family: ubuntu;
 
   height: 1.5rem;
   text-align: left;
+  .left p {
+    font-size: 0.9rem;
+    margin-left: 1rem;
+    a {
+      color: #4285f4;
+      text-decoration: none;
+    }
+  }
+
+  .right p {
+    font-size: 1rem;
+    margin-right: 1rem;
+  }
 `;
