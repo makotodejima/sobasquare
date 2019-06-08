f91812d0131e4dd633bd663261e9a2504be7ae77
Makoto Dejima <makotodejima@gmail.com>
2019-06-09 00:09:44 +0200

    react-ga

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import { Provider } from "react-redux";
 import reducers, { middleware } from "./reducers/reducer";
 import "./styles.scss";
 import List from "./components/List";
+import ReactGA from "react-ga";
+
+// Using react-ga
+ReactGA.initialize("UA-141671732-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export const store = createStore(reducers, middleware);
 
