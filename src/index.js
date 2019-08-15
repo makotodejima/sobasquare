542dd0e98f61a86d02407f4e76ee5c2e8a2d011b
Makoto Dejima <makotodejima@gmail.com>
2019-08-15 15:04:06 +0200

    pkg

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import { Provider } from 'react-redux';
 import reducers, { middleware } from './reducers/reducer';
 import './styles.scss';
 import Router from './components/Router';
-import ReactGA from 'react-ga';
-
-// Using react-ga
-ReactGA.initialize('UA-141671732-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 // Redux devtool
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
